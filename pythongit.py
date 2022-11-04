8a48a337e4704c4ed0f240e3611bb2e78d0abb14
saloni3388 <86301560+saloni3388@users.noreply.github.com>
2022-11-04 09:19:24 +0530

    Update pythongit.py

--- pythongit.py
+++ pythongit.py
@@ -1,10 +1,10 @@
-n = 5
-if n > 1:
-	for i in range(2, n):
-		if (n % i) == 0:
-			print(n, "is not a prime number")
-		break
-	else:
-		print(n, "is a prime number")
+num = 11
+if num > 1:
+    for i in range(2, num):
+        if (num % i) == 0:
+            print(num, "is not a prime number")
+            break
+        else:
+            print(num, "is a prime number")
 else:
-	print(n, "is not a prime number")
+    print(num, "is not a prime number")
